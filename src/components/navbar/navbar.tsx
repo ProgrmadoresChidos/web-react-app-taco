b3622d88a2213d8bc7222f2a18fd8a754861e5b8
Roberto Valdez <robert.tito94@gmail.com>
2020-07-21 10:14:28 -0500

    maquetar navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import "./navbar.css";
 
-export function navbar() {
+const Navbar = () => {
+
+    const handleClick = (cadena : string) => {
+        console.info( cadena );
+    }
+
     return (
         <div>
-            
+        <div className="navbar navbar--background">
+            <div className="navbar__navOption navbar--background" onClick={ () => handleClick('Inicio') }>
+                <p className="navbar__text">Inicio</p>
+            </div>
+            <div className="navbar__navOption navbar--background" onClick={ () => handleClick('Ubicación y horarios') }>
+                <p className="navbar__text">Ubicación & Horarios</p>
+            </div>
+            <div className="navbar__navOption navbar--background" onClick={ () => handleClick('Manú') }>
+                <p className="navbar__text">Menú</p>
+            </div>
         </div>
-    )
-}
+        </div>
+    );
+};
+
+export default Navbar;
