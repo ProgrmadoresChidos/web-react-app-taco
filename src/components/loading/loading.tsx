6cd02c237592ca351360048976ae9b67b821c020
Roberto Valdez <robert.tito94@gmail.com>
2020-07-24 14:49:06 -0500

    clean code

--- src/components/loading/loading.tsx
+++ src/components/loading/loading.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense, Children } from "react";
+import React, { Suspense } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
 
