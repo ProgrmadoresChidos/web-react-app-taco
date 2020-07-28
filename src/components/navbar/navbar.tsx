045b343a7baece3ee7c542eb056197a7faf77f99
Programadores Chidos <dvLandSoft@gmail.com>
2020-07-28 16:11:53 -0500

    borrar comentarios

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -4,23 +4,15 @@ import style from "./navbar.module.css";
 
 const Navbar = () => {
 
-    // const handleClick = (cadena: string) => {
-    //     console.info(cadena);
-    // }
-
     return (
         <header className={ `${style.navbar} ${style.navbar_background}` }>
             <div className={ style.navbar__contentWrapper }>
-                {/* <div className={ `${style.navbar__navOption} ${style.navbar_background} ` } onClick={() => handleClick('Inicio')}> */}
                 <NavLink exact to="/" className={ `${style.navbar__navOption} ${style.navbar_background} `} activeClassName={style.navbar_activated}>
                     <p className={ style.navbar__text }>Inicio</p>
                 </NavLink>
-                {/* </div> */}
-                {/* <div className={ `${style.navbar__navOption} ${style.navbar__background} ${style.navbar_activated}` } onClick={() => handleClick('Ubicación y horarios')}> */}
                 <NavLink exact to="/ubicacion" className={ `${style.navbar__navOption} ${style.navbar__background}` } activeClassName={style.navbar_activated}>
                     <p className={ style.navbar__text }> Ubicación & Horarios</p>
                 </NavLink> 
-                {/* </div> */}
                 <NavLink exact to="/menu" className={ `${style.navbar__navOption} ${style.navbar_background}`} activeClassName={style.navbar_activated}>
                     <p className={ style.navbar__text }>Menú</p>
                 </NavLink>
