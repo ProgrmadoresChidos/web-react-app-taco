531aa26faf624533f3b621fc48c80bf1ec016c04
Roberto Valdez <robert.tito94@gmail.com>
2020-07-30 17:19:52 -0500

    clean code

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import './App.css';
 
-// import Footer from './components/footer/footer'
-// import Navbar from './components/navbar/navbar';
 import Routes from './routes/Routes';
 
 function App() {
