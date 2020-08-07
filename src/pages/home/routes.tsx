7722f42786be9adaecdd6ff0a870b9f00395062e
Nestor <nestor_gm02@hotmail.com>
2020-08-07 18:16:30 -0500

    se extrajo el component routes al componente Router

--- src/pages/home/routes.tsx
+++ src/pages/home/routes.tsx
@@ -1,17 +1,33 @@
 import React from 'react'
-import { Switch } from 'react-router-dom';
+// import { Switch } from 'react-router-dom';
 import Index from './spa-pages/main-page/index';
-import Route from '../../routes/Route';
+// import Route from '../../routes/Route';
 import Ubication from './spa-pages/ubication-more-info/ubication';
 import { Menu } from './spa-pages/menu/menu';
+import Router from '../../routes/Router';
 
 export default function Routes({ path }) {
-
+    const ROUTES = [
+        {
+            path: `${path}`,
+            exact: true,
+            component: Index,
+        },
+        {
+            path: `${path}/ubicacion`,
+            component: Ubication,
+        },
+        {
+            path: `${path}/menu`,
+            component: Menu,
+        },
+    ]
     return (
-        <Switch>
-            <Route path={`${path}`} exact component={Index} />
-            <Route path={`${path}/ubicacion`} component={Ubication} />
-            <Route path={`${path}/menu`} component={Menu} />
-        </Switch>
+        // <Switch>
+        //     <Route path={`${path}`} exact component={Index} />
+        //     <Route path={`${path}/ubicacion`} component={Ubication} />
+        //     <Route path={`${path}/menu`} component={Menu} />
+        // </Switch>
+        <Router routes={ROUTES} />
     )
 }
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,20 +1,42 @@
 import React from 'react'
-import { Switch } from 'react-router-dom';
+// import { Switch } from 'react-router-dom';
+import Router from './Router';
 //RoutesWapper
-import Route from './Route';
+// import Route from './Route';
 
 // pages
 import Login from '../pages/login';
 import Pages from '../pages/home';
 import PageNotFound from '../pages/page-not-found';
 
+const ROUTES = [
+    {
+        path: "/",
+        redirect: "/inicio",
+        exact: true,
+        component: () => { },
+    },
+    {
+        path: "/inicio",
+        component: Pages,
+    },
+    {
+        path: "/login",
+        component: Login,
+    },
+    {
+        component: PageNotFound,
+    },
+]
+
 export default function Routes() {
     return (
-        <Switch>
-            <Route path="/" redirect="/inicio" exact component={() => { }} />
-            <Route path="/inicio" component={Pages} />
-            <Route path="/login" component={Login} />
-            <Route component={PageNotFound} />
-        </Switch>
+        // <Switch>
+        //     <Route path="/" redirect="/inicio" exact component={() => { }} />
+        //     <Route path="/inicio" component={Pages} />
+        //     <Route path="/login" component={Login} />
+        //     <Route component={PageNotFound} />
+        // </Switch>
+        <Router routes={ROUTES} />
     )
 }
