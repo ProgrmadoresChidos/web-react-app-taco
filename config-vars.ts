4161429462ebec22ad2fd5a9adaeb8ebb1eee105
Nestor <nestor_gm02@hotmail.com>
2020-08-13 17:12:50 -0500

    funcion para escribir las variables de entorno en el .env

--- config-vars.ts
+++ config-vars.ts
@@ -1,6 +1,8 @@
 const { execSync } = require('child_process');
+const { writeFileSync } = require('fs');
 
-let resp = execSync('heroku config --app=la-app-del-taco-web');
+// let resp = execSync('heroku config --app=la-app-del-taco-web');
+let resp = execSync('heroku config --app=nestor-webserver-curso-node');
 resp = resp.toString();
 const info = resp.split('\n');
 let configVars = {};
@@ -17,4 +19,17 @@ configVars = info.reduce((resp, element, index) => {
   return resp;
 }, {});
 
-console.log(configVars);
+let file = '';
+const keys = Object.keys(configVars);
+keys.forEach((key, index,) => {
+  file = `${file}${key}=${configVars[key]}`;
+  if (index !== keys.length - 1) {
+    file = `${file}\n`;
+  }
+});
+try {
+  writeFileSync('.env', file);
+} catch (error) {
+  throw error;
+}
+console.log('Se escribio .env correctamente');
